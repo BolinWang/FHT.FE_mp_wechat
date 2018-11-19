8f8bf3aa6ae324bc5e50f113f27c600fe7998ace
dengdongming <ddm@fuhengkeji.com>
2018-11-19 16:09:08 +0800

    OTA  小程序

---
+++ ota/pages/login/login.js
@@ -0,0 +1,152 @@
+// pages/login/login.js
+const fetch = require('../../utils/api.js')
+import { SHA2 } from '../../utils/shaEncrypt.js'
+import { validateMobile } from '../../utils/validate.js'
+const app = getApp()
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list: [{
+      id: '0',
+      title: '密码登录'
+    }, {
+      id: '1',
+      title: '验证码登录'
+    }],
+    tabActive: '0',
+    mobile: null,
+    textTime: '获取验证码',
+    codeDis: false,
+    vcode: null,  //验证码
+    password: null,
+    username: null,
+    activeInput: null
+  },
+  goLogin() {   //账号密码登陆
+    if (!this.data.username) {
+      wx.showToast({
+        icon: 'none',
+        title: '手机号不能为空',
+      })
+    } else if (!this.data.password) {
+      wx.showToast({
+        icon: 'none',
+        title: '请输入密码',
+      })
+    } else if (!validateMobile(this.data.username)) {
+      wx.showToast({
+        icon: 'none',
+        title: '手机号码格式不正确',
+      })
+    } else {
+      this.submitLogin()
+    }
+
+  },
+  submitLogin() {
+    let that = this
+    fetch('/customer', {
+      method: 'loginByPassword',
+      params: {
+        username: that.data.username,
+        password: SHA2(that.data.password)
+      }
+    }).then(res => {
+      if (res.code == 0) {
+        wx.showToast({
+          icon: 'none',
+          title: '登录成功',
+        })
+        wx.setStorage({
+          key: 'MLZFUSERNAME',
+          data: that.data.username,
+        })
+        wx.setStorageSync('OTA_sessionId', res.data.sessionId)
+        app.globalData.sessionId = res.data.sessionId
+        wx.redirectTo({
+          url: '../personalCenter/personalCenter'
+        })
+      }
+    })
+  },
+  // 获取手机号码
+  getMobile(e) {
+    this.setData({
+      mobile: e.detail.value.replace(/\s+/g, '')
+    })
+  },
+  getUsername(e) {
+    this.setData({
+      username: e.detail.value.replace(/\s+/g, '')
+    })
+  },
+  //获取密码
+  getPassword(e) {
+    this.setData({
+      password: e.detail.value.replace(/\s+/g, '')
+    })
+  },
+  tapName(e) {
+    const tabId = e.currentTarget.dataset.id;
+    this.setData({
+      tabActive: tabId
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
--- ota/utils/api.js
+++ ota/utils/api.js
@@ -5,7 +5,13 @@ const defaultConfig = {
   sign: "8F4C4A8E9D850EDD9692DE38723D0543"
 }
 // basePath api请求路径
-const basePath = 'https://dev.mdguanjia.com/bop/'
+
+
+//测试
+const basePath = 'https://test.mdguanjia.com/otastarter/'
+
+// 线上
+// const basePath = 'https://api.mdguanjia.com/otastarter'
 
 const fetch = (url, data, params = {}) => {
   wx.showLoading({
@@ -15,38 +21,38 @@ const fetch = (url, data, params = {}) => {
     let that = this
     let postData = Object.assign(data, defaultConfig)
     // 登录不需要做sessionId鉴权
-    if (postData.method != 'login') {
-      postData.sessionId = that.globalData.sessionId
+    if (postData.method !== 'login') {
+      postData.sessionId = wx.getStorageSync('sessionId')
     }
     wx.request({
       url: basePath + url,
       data: postData,
-      method: 'POST',
+      method: params.method || 'POST',
       header: { 'content-type': 'application/json' },
       success: function (res) {
         setTimeout(function () {
           wx.hideLoading()
         }, 1000)
         // 响应业务分析
-        // if (res.data.code == 0) {
-        //   resolve(res.data.data)
-        // } else if (res.data.code == 1016) {
-        //   wx.removeStorage({
-        //     key: 'BOP_ADMIN',
-        //     success: function (res) {
-        //       wx.reLaunch({
-        //         url: '/pages/login/login'
-        //       })
-        //     }
-        //   })
-        // } else {
-        //   wx.showToast({
-        //     title: res.data.message || '网络异常',
-        //     icon: 'none',
-        //     duration: 2000
-        //   })
-        //   reject(res.data.message)
-        // }
+        if (res.data.code == 0) {
+          resolve(res.data.data)
+        } else if (res.data.code == 1016) {
+          wx.removeStorage({
+            key: 'OTA_sessionId',
+            success: function (res) {
+              wx.reLaunch({
+                url: '/pages/login/login'
+              })
+            }
+          })
+        } else {
+          wx.showToast({
+            title: res.data.message || '网络异常',
+            icon: 'none',
+            duration: 2000
+          })
+          reject(res.data.message)
+        }
       },
       error: function (e) {
         reject('网络出错')
